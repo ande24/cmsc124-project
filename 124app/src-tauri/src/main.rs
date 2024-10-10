c22f5b5e40057657d47200e4dc25123c4a8e2d00
AndeeLlenes <62787620+AndeeLlenes@users.noreply.github.com>
2024-10-10 09:53:12 +0800

    pinakalast

--- 124app/src-tauri/src/main.rs
+++ 124app/src-tauri/src/main.rs
@@ -1,18 +1,8 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use tauri::{Manager, Window};
-
-#[tauri::command]
-fn close_splashscreen(app: tauri::AppHandle) {
-    if let Some(splashscreen) = app.get_window("splashscreen") {
-        splashscreen.close().unwrap();
-    }
-}
-
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![close_splashscreen])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- 124app/src/main.js
+++ 124app/src/main.js
@@ -1,6 +1,4 @@
 import { createApp } from "vue";
-import { appWindow } from '@tauri-apps/api/window';
-import { invoke } from '@tauri-apps/api/tauri';
 
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
@@ -8,17 +6,6 @@ import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
 import App from "./App.vue";
-
-appWindow.on('tauri://ready', () => {
-    // Simulate a delay or wait for actual initialization to complete
-    setTimeout(() => {
-      // Hide the splash screen
-      invoke('close_splashscreen');
-  
-      // Show the main window
-      appWindow.show();
-    }, 3000); // Delay of 2 seconds for the splash screen
-  });
 
 const vuetify = createVuetify({
     components,
